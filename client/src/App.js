f0279eca86565edf6b6e8c051898371dbe08647f
Dima <benensondimitrii@gmail.com>
2022-06-19 13:28:05 +0300

    updated login page and logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,51 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 
+const initialState = {
+  user: "",
+};
+
 function App() {
-  useEffect(() => {}, []);
+  const [value, setValue] = useState(initialState);
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      setIsLogin(true);
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitLogin = (e) => {
+    e.preventDefault(e.target);
+    if (value.user) {
+      setIsLogin(true);
+    }
+    localStorage.setItem("user", value.user);
+  };
+
+  const logout = () => {
+    setValue(initialState);
+    localStorage.setItem("user", initialState.user);
+
+  };
 
   return (
     <div className="App">
-      <LoginPage />
-      {/* <MainPage /> */}
+      {!isLogin ? (
+        <LoginPage
+          onSubmitLogin={onSubmitLogin}
+          handleChange={handleChange}
+          user={value.user}
+        />
+      ) : (
+        <MainPage user={value.user} logout={logout} />
+      )}
     </div>
   );
 }
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const LoginPage = () => {
+const LoginPage = ({ user, handleChange, onSubmitLogin }) => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div>
+      <div>
+        {" "}
+        <label className="form-label">User Name</label>
+        <input
+          type="text"
+          name="user"
+          value={user}
+          onChange={handleChange}
+          className="form-input"
+        ></input>
+        <button type="submit" className="btn btn-block" onClick={onSubmitLogin}>
+          Submit{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
